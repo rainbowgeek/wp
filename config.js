e62b2c414373b177d7763f5195a368beb7577266
rainbowgeek <b.denis@novaldi.fr>
2016-05-10 11:19:32 +0200

    WPAK 1

---
+++ config.js
@@ -0,0 +1,19 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'agenda-ios',
+		wp_ws_url : 'http://localhost/agenda_wp/wp-appkit-api/agenda-ios',
+		theme : 'wpak-off-canvas-ios',
+		version : '1.0',
+		app_title : 'Agenda',
+		app_platform : 'ios',
+		gmt_offset : 2,
+		debug_mode : 'off',
+		auth_key : '|mUSE(Kh7t?L+I{= ,/^eQ26cO`[J+ef7Sg{h~a/Z-SiVke)-pr+&9|,MYF7B()2',
+		options : {"refresh_interval":0},
+		addons : []
+	};
+
+});
