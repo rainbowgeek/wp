22cbabda0fe2de683ccb01924d85405faacac190
rainbowgeek <b.denis@novaldi.fr>
2016-06-01 17:20:23 +0200

    WPAK15

---
+++ config.js
@@ -0,0 +1,19 @@
+define( function ( require ) {
+
+	"use strict";
+
+	return {
+		app_slug : 'agenda-ios',
+		wp_ws_url : 'http://agendacommune.novaldi.fr/wp-appkit-api/agenda-ios',
+		theme : 'q-ios-1.0',
+		version : '1.0',
+		app_title : 'Agenda',
+		app_platform : 'ios',
+		gmt_offset : 2,
+		debug_mode : 'off',
+		auth_key : '|mUSE(Kh7t?L+I{= ,/^eQ26cO`[J+ef7Sg{h~a/Z-SiVke)-pr+&9|,MYF7B()2',
+		options : {"refresh_interval":0},
+		addons : []
+	};
+
+});
